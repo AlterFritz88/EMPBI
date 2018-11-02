f929ccd7656d79a4f99bb0676478d47bab40433f
Sashko <burdin009@gmail.com>
2018-11-02 13:43:45 +0300

    еще пофиксил баг с модификацией 4 режима
    Signed-off-by: Sashko <burdin009@gmail.com>

--- modi.py
+++ modi.py
@@ -103,6 +103,7 @@ while i < all_data:
                     if preper_data[new_i][5:] == '11111111111':  # it means FG
 
                         if (preper_data[new_i][1:4] == '000') and in_process == 0:  # it means mode 000
+
                             in_process = 1
                             uart_data.append('10000010')  # reset
                             uart_data.append('10100100')  # byte to say
@@ -120,7 +121,6 @@ while i < all_data:
                         if (preper_data[new_i][1:4] == '001') and in_process == 0:  # it means mode 001
                             in_process = 1
                             groups = int(preper_data[new_i + 1], 2) * 5
-                            # print(groups)
                             new_i += 2
                             uart_data.append('10010100')  # byte to say
                             uart_data.append(hex2bin(str(hex(groups)), 16)[8:])
@@ -129,7 +129,7 @@ while i < all_data:
                             for j in range(groups):
                                 uart_data.append(preper_data[new_i + j][0:8])
                                 uart_data.append(preper_data[new_i + j][8:])
-                            new_i += groups
+                            new_i += groups - 1
 
                         if (preper_data[new_i][1:4] == '010') and in_process == 0:  # it means mode 010
                             in_process = 1
@@ -151,10 +151,10 @@ while i < all_data:
                             uart_data.append(hex2bin(str(hex(groups)), 16)[8:])
                             uart_data.append(preper_data[new_i][0:8])  # initial adr memory
                             uart_data.append(preper_data[new_i][8:])
-                            new_i += groups
+                            new_i += groups + 1
                     new_i += 1
                     in_process = 0
-        #limited = False
+        limited = False
     i += 1
     in_process = 0
 print(preper_data)
