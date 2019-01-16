5a67774388028331b0ac6d569d75a5c1f911c587
Sashko <burdin009@gmail.com>
2019-01-16 14:24:05 +0300

    сделал мультитренд лоя отображения инфы
    Signed-off-by: Sashko <burdin009@gmail.com>

--- driver.py
+++ driver.py
@@ -4,11 +4,37 @@ from modi import recive_pp
 from to_binary import hex2bin
 #sudo chmod a+rw /dev/tty
 
+def etap_processor(etalons, data):
+    ser = serial.Serial('/dev/ttyUSB0', baudrate=1000000, timeout=0.5)
+    data_b = [bytes([int(x, 2)]) for x in data]
+    etalon = len(etalons) * 2
+    answer_etap = []
+    for inf in data_b:
+        if inf == b'\x81':
+            ser.write(inf)
+            response = b'\x00'
+            while response != b'\xff':
+                print('wait answer from block')
+                response = ser.read(1)
+                print(response)
+        ser.write(inf)
+    for i in range(etalon):
+        answer_etap.append(ser.read(1))  # len(etalons[etap])
+    answer_etap_res = []
+    for i in range(len(answer_etap)):
+        if i % 2 == 1:
+            continue
+        else:
+            answer_etap_res.append(hex2bin(str(int.from_bytes(answer_etap[i], byteorder='big')), 10)[7:] + hex2bin(
+                str(int.from_bytes(answer_etap[i + 1], byteorder='big')), 10)[7:])
+
+    return answer_etap_res  # int.from_bytes(i, byteorder='big')
 
 
 
 def do_prog(start_etap=0, end_etap=-1):
     def etap_processor(etalons, data, etap, step, mod=None):
+        ser = serial.Serial('/dev/ttyUSB0', baudrate=1000000, timeout=0.2)
         data_b = [bytes([int(x, 2)]) for x in data]
         etalon = len(etalons) * 2
         answer_etap = []
@@ -61,3 +87,41 @@ def do_prog(start_etap=0, end_etap=-1):
                     etalon_bin.append(((etap, step, mod), etalon_s_e))
 
     return answer, etalon_bin
+
+
+def get_info_for_send():
+    to_send, etalons, etaps = recive_pp()
+    data_for_return = []
+
+    for etap in etaps:
+        data_temp = to_send[etap]
+        etalon = etalons[etap]
+
+        for step in range(len(data_temp)):
+
+            if type(data_temp[step][0]) == str:
+                print(etap, step, data_temp[step])
+                etalon_s_e = etalon[step]
+                data_for_return.append([(etap, step), data_temp[step], etalon_s_e])
+
+
+
+            else:
+                for mod in range(len(data_temp[step])):
+                    print(etap, step, mod, data_temp[step][mod])
+                    etalon_s_e = etalon[step][mod]
+                    data_for_return.append([(etap, step, mod), data_temp[step][mod], etalon_s_e])
+
+    return data_for_return
+
+
+
+
+if __name__ == '__main__':
+    data = get_info_for_send()
+    print('datat', data)
+    for i in data:
+        print('i', i)
+        get_ans = etap_processor(i[-1], i[1])
+        i.append(get_ans)
+    print(data)
--- kivy_ui.py
+++ kivy_ui.py
@@ -6,6 +6,11 @@ from kivy.uix.pagelayout import PageLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.dropdown import DropDown
+from kivy.clock import Clock
+from driver import do_prog, get_info_for_send, etap_processor
+import time
+
+import threading
 
 
 from kivy.lang import Builder
@@ -22,24 +27,64 @@ class EMPBI(App):
     import time
     #self.root.ids._label.ids.mail_label.text = str(app.time.time())
     count_cirkles = 1
+    my_data = []
+    screen_data =[]
+    screen_etalons = []
+    potok = None
+
     def build(self):
         #Main.ids._label.ids.mail_label.text = str(self.time.time())
 
+        #Clock.schedule_interval(self.show, 0.1)
+        #print(Main.ids._main)
+        #Clock.schedule_interval(self.do_program, 0.5)
         return Main()
 
 
     def print_out(self):
         print(self.count_cirkles)
-        print(self.root.ids._label.ids.mail_label.text)
+        print(self.root.ids._label.ids._data.adapter.data)
+
 
     def do_program(self):
-        from driver import do_prog
-        response = []
+        co = 0
+        data = get_info_for_send()
+
         for i in range(self.count_cirkles):
-            response.append(do_prog())
-            print('отвкет',response[0][1][0][1][0])
-            self.root.ids._label.ids.mail_label.text = response[0][1][0][1][0]
-        print(len(response))
+            for j in data:
+
+                get_ans = etap_processor(j[-1], j[1])
+                self.screen_data = get_ans
+                self.screen_etalons = [x + str(time.time()) for x in j[-1]]
+                time.sleep(0.3)
+                print('count do:', co)
+                print(get_ans)
+                co += 1
+
+                #Clock.schedule_interval(self.show, 0.1)
+                self.show()
+
+
+
+
+    def upd_ltxt(self):
+        self.potok = threading.Thread(target=self.do_program)
+        self.potok.start()
+
+
+
+
+
+    def show(self):
+        self.root.ids._label.ids._income_data.adapter.data = self.screen_data
+        self.root.ids._label.ids._etalon_data.adapter.data = self.screen_etalons
+        print('показываю')
+        #print(self.root.ids._label.ids._income_data.adapter.data)
+
+
+    #done(data, self.count_cirkles)
+
+
 
 
 
